e48506c048739e7aba6beac82cfd0936beb3861b
Fredrik Salomonsson <plattfot@gmail.com>
2015-11-22 20:06:47 -0800

    Cleaned up the code, fixed a bug in the charging computation. It was using full design but if using a worn out battery it will never reach that charge. So switched to using the latest known full charge. Added command line options --path and --threshold. With --path you can specify another location to look for the batteries if they are not in /sys/class/power_supply. The --threshold option will set when i3blocks should color the block red i.e. notify the user that all batteries are soon too be depleted. Default is 10.

--- battery.cpp
+++ battery.cpp
@@ -19,15 +19,25 @@ enum Status : size_t {
   ST_PLUG = 2
 };
 
-std::string STATUS_ICONS[] = {
+static const std::string STATUS_ICONS[] = {
   " ",
   "",
   ""
 };
 
+typedef std::array<std::string,5> ArrayT;
+struct Parameters 
+{
+  std::string path = "/sys/class/power_supply";
+  ArrayT icons = ArrayT{ "", "", "", "", "" };
+  long int battery = -1; // -1 sum all.
+  double threshold = 10.0;
+};
+
 struct BatteryData {
   Status status = ST_PLUG;
   double full_design = -1.0;
+  double full = -1.0;
   double remaining = -1.0;
   double present_rate= -1.0;
   double voltage = -1.0;
@@ -35,14 +45,23 @@ struct BatteryData {
 };
 
 typedef std::vector<BatteryData> BatteryVectorT;
-typedef std::array<std::string,5> ArrayT;
 
-struct Parameters 
+BatteryData combineBatteries( const BatteryVectorT& batteries )
 {
-  std::string path = "/sys/class/power_supply";
-  ArrayT icons = ArrayT{ "", "", "", "", "" };
-  long int battery = -1; // -1 sum all.
-};
+  BatteryData data;
+  data.remaining = 0.0;
+  data.full_design = 0.0;
+  data.full = 0.0;
+  for( const auto& battery : batteries ) {
+    data.status = std::min( data.status, battery.status );
+    data.remaining += battery.remaining;
+    data.full_design += battery.full_design;
+    data.full += battery.full;
+    data.present_rate = std::max( data.present_rate, battery.present_rate );
+    data.voltage = std::max( data.voltage, battery.voltage );
+  }
+  return data;
+}
 
 size_t countBatteries(const std::string& path) 
 {
@@ -107,6 +126,8 @@ void parseBattery( const std::string& path, BatteryData& data )
           }
         } else if (begins_with("POWER_SUPPLY_ENERGY_FULL_DESIGN", delim)){
           data.full_design = to_double( delim );
+        } else if (begins_with("POWER_SUPPLY_ENERGY_FULL", delim)){
+          data.full = to_double( delim );
         }
       }
       file.close();
@@ -116,11 +137,13 @@ void parseBattery( const std::string& path, BatteryData& data )
 void convertTo_mWh( BatteryData& data )
 {
   // From print_battery_info.c in i3status
-  data.present_rate = (data.voltage / 1000.0) * (data.present_rate / 1000.0);
-
   if( data.voltage != -1) {
-    data.remaining = (data.voltage / 1000.0) * (data.remaining / 1000.0);
-    data.full_design = (data.voltage / 1000.0) * (data.full_design / 1000.0);
+    const double mV = data.voltage / 1000.0;  
+
+    data.present_rate = mV * (data.present_rate / 1000.0);
+    data.remaining = mV * (data.remaining / 1000.0);
+    data.full_design = mV * (data.full_design / 1000.0);
+    data.full = mV * (data.full / 1000.0);
   }
 }
 
@@ -129,7 +152,7 @@ void computeTime( const BatteryData& data, std::stringstream& ss )
   double remaining_time;
 
   if( data.status == ST_CHARGING) {
-    remaining_time = (data.full_design - data.remaining) / data.present_rate;
+    remaining_time = (data.full - data.remaining) / data.present_rate;
   } else if ( data.status == ST_DISCHARGING ) {
     remaining_time = data.remaining / data.present_rate;
   } else {
@@ -141,25 +164,9 @@ void computeTime( const BatteryData& data, std::stringstream& ss )
   const size_t hours = seconds_remaining / 3600;
   const size_t seconds = seconds_remaining - (hours * 3600);
   const size_t minutes = seconds / 60;
+  
   ss<<(hours<10?"0":"")<<hours<<":"<<(minutes<10?"0":"")<<minutes;
 }
-
-void batteryStatus( const BatteryVectorT& batteries,
-                    const Parameters& params,
-                    std::stringstream& ss )
-{
-  if( params.battery == -1 ){
-    Status status = ST_DISCHARGING;
-    // Compute the total percentage
-    for( const auto& data : batteries ){
-      status = std::min(status, data.status);
-    }
-    ss<<STATUS_ICONS[ status ];
-  } else {
-    ;
-  }
-}
-
 
 bool parseArgs( const int argc, char* const* argv, Parameters& params )
 {
@@ -169,8 +176,11 @@ bool parseArgs( const int argc, char* const* argv, Parameters& params )
       {"custom",  required_argument, 0, 'c'},
       {"help",  no_argument, 0, 'h'},
       {"battery",  no_argument, 0, 'b'},
+      {"path", required_argument, 0, 'p'},
+      {"threshold", required_argument, 0, 'T'},
       {0, 0, 0, 0}
     };
+
   static const std::string help_text = R"(
 Options:
   -t, --type [TYPE]         Specify what icons to use to indicate 
@@ -178,8 +188,13 @@ Options:
                             heart. Battery is the default.
   -c, --custom [FULL,EMPTY] Use custom battery indicator, using a 
                             combination  of characters FULL and EMPTY. 
-  -b, --battery [-1,0,...]  Specify what battery to monitor. -1 combines
+  -b, --battery [INT]       Specify what battery to monitor. -1 combines
                             all the batteries and is the default.
+  -T, --threshold [INT]     When the battery percentage falls under this 
+                            threshold it will color the block red.
+                            Default is 10.
+  -p, --path [PATH]         Specify path to where info on the batteries are 
+                            stored, default is /sys/class/power_supply.
   -h, --help                Print this message and then exit.
 Author:
   Fredrik "PlaTFooT" Salomonsson
@@ -187,7 +202,7 @@ Author:
   int option_index = 0;
   int c;
   while( true ) {
-    c = getopt_long (argc, argv, "t:c:b:h", long_options, &option_index);
+    c = getopt_long (argc, argv, "t:c:b:T:p:h", long_options, &option_index);
     
     /* Detect the end of the options. */
     if (c == -1)
@@ -226,6 +241,12 @@ Author:
       // Using strol for more robust conversion than atoi
       params.battery = strtol(optarg,NULL,10);
       break;
+    case 'p':
+      params.path = optarg;
+      break;
+    case 'T':
+      params.threshold = static_cast<double>( strtol( optarg, NULL, 10 ) );
+      break;
     case 'h':
       std::cout<<"Usage: "<<argv[0]<<" [OPTIONS]..."<<help_text;
       return false;
@@ -235,56 +256,42 @@ Author:
   return true;
 }
 
-bool processButtons( const BatteryVectorT& batteries,
+size_t processButtons( const BatteryVectorT& batteries,
                      const size_t num_batts,
                      Parameters& params )
 {
   std::string block_button;
   {
     char* block_buffer = getenv("BLOCK_BUTTON");
-    block_button = block_buffer != NULL ? block_buffer : "";
+    block_button = block_buffer != NULL ? std::move( block_buffer ) : "";
   }
+
   if( !block_button.empty() ){
     // Handle button event
     if( block_button == "1" )
-      return true;
-    else if( block_button == "3"){
+      return 1;
+    else {
+      const size_t button = strtoul( block_button.c_str(), NULL, 10 );
       // Find the first battery with energy remaining, starting from
       // the back.
       for( auto it = batteries.crbegin(), end = batteries.crend();
            it != end; ++it ){
         if( it->remaining > 0.0 ) {
           params.battery = (end - it) - 1;
-          return false;
+          return button;
         }
       }
     }
   }
-  return false;
-}
-
-BatteryData combineBatteries( const BatteryVectorT& batteries )
-{
-  BatteryData data;
-  data.remaining = 0.0;
-  data.full_design = 0.0;
-  for( const auto& battery : batteries ) {
-    data.status = std::min( data.status, battery.status );
-    data.remaining += battery.remaining;
-    data.full_design += battery.full_design;
-    data.present_rate = std::max( data.present_rate, battery.present_rate );
-    data.voltage = std::max( data.voltage, battery.voltage );
-  }
-  return data;
+  return 0;
 }
 
 int main( int argc, char** argv )
 {
-  const double threshold = 10.0;
   Parameters params;
 
   if( !parseArgs(argc,argv,params) )
-    return 0;
+    return 1;
 
   const long int num_batts = countBatteries( params.path );
   // Make sure battery index is within bounds
@@ -312,7 +319,8 @@ int main( int argc, char** argv )
     }
   }
 
-  const bool button1_pressed = processButtons( batteries, num_batts, params );
+  // Returns 0 if no button is pressed
+  const size_t button = processButtons( batteries, num_batts, params );
 
   const BatteryData& data = params.battery == -1 
     ? combineBatteries( batteries ) 
@@ -321,26 +329,36 @@ int main( int argc, char** argv )
   const double percentage = data.remaining / data.full_design * 100.0;
 
   std::stringstream ss;
-
+  switch( button ) {
+  case 2:
+  case 3:
+    ss<<params.battery<<":";
+    break;
+  default:
+    ss<<STATUS_ICONS[ data.status ]<<" ";
+  }
+  
   // Add battery status 
-  ss<<STATUS_ICONS[ data.status ];
-
-  if( button1_pressed ) {
+  switch( button ) {
+  case 1:
+  case 2:
     ss<<static_cast<size_t>(percentage)<<"% ";
-  } else {
-    ss<<" ";
-    if( percentage >= 95.0 ) {
-      ss<<params.icons[0];
-    } else if( percentage >= 75.0 ) {
-      ss<<params.icons[1];
-    } else if( percentage >= 50.0 ) {
-      ss<<params.icons[2];
-    } else if( percentage >= 25.0 ) {
-      ss<<params.icons[3];
-    } else {
-      ss<<params.icons[4];
-    }
-    ss<<"  ";
+    break;
+  default:
+      {
+        if( percentage >= 95.0 ) {
+          ss<<params.icons[0];
+        } else if( percentage >= 75.0 ) {
+          ss<<params.icons[1];
+        } else if( percentage >= 50.0 ) {
+          ss<<params.icons[2];
+        } else if( percentage >= 25.0 ) {
+          ss<<params.icons[3];
+        } else {
+          ss<<params.icons[4];
+        }
+        ss<<" ";
+      }
   }
   
   // Compute time of discharge/charge
@@ -350,12 +368,11 @@ int main( int argc, char** argv )
     ss<<"Full";
   }
 
-
   // Output the format i3blocks want
   std::cout<<ss.str()<<std::endl;
   std::cout<<ss.str()<<std::endl;
 
-  if( percentage <= threshold )
+  if( percentage <= params.threshold )
     std::cout<<"#FF0000"<<std::endl;
   
   return 0;
